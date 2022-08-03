6d2c2bf061cc8c16c6351030f6e019e01c8b1e9b
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-08-03 13:22:24 -0300

    pokebola en card pokemon

--- FrontEnd/src/components/Characters/EachCharacter.jsx
+++ FrontEnd/src/components/Characters/EachCharacter.jsx
@@ -19,22 +19,139 @@ function EachCharacter(props) {
 
   // CSS para cada TYPE
   const classType = props.typePokemon.map((type, i) => {
-    if(type === "poison") {
-      return <div className={classes.typePoison} key={i}><p>{type}</p></div>
-    } else if (type === "grass") {
-      return <div className={classes.typeGrass} key={i}><p>{type}</p></div>
-    } else if (type === "fire") {
-      return <div className={classes.typeFire} key={i}><p>{type}</p></div>
-    } else if (type === "water") {
-      return <div className={classes.typeWater} key={i}><p>{type}</p></div>
-    } else if (type === "flying") {
-      return <div className={classes.typeFlying} key={i}><p>{type}</p></div>
-    } else if (type === "electric") {
-      return <div className={classes.typeElectric} key={i}><p>{type}</p></div>
-    } else {
-      return <div className={classes.typeOthers} key={i}><p>{type}</p></div>
+    if (type === "poison") {
+      return (
+        <div className={classes.typePoison} key={i}>
+          <p>{type}</p>
+        </div>
+      );
     }
-  })
+
+    if (type === "grass") {
+      return (
+        <div className={classes.typeGrass} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+
+    if (type === "fire") {
+      return (
+        <div className={classes.typeFire} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+
+    if (type === "water") {
+      return (
+        <div className={classes.typeWater} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+
+    if (type === "flying") {
+      return (
+        <div className={classes.typeFlying} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+
+    if (type === "electric") {
+      return (
+        <div className={classes.typeElectric} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+
+    if (type === "bug") {
+      return (
+        <div className={classes.typeBug} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "normal") {
+      return (
+        <div className={classes.typeNormal} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "ground") {
+      return (
+        <div className={classes.typeGround} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "fairy") {
+      return (
+        <div className={classes.typeFairy} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "fighting") {
+      return (
+        <div className={classes.typeFighting} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "psychic") {
+      return (
+        <div className={classes.typePsychic} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "rock") {
+      return (
+        <div className={classes.typeRock} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "steel") {
+      return (
+        <div className={classes.typeSteel} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "ice") {
+      return (
+        <div className={classes.typeIce} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "ghost") {
+      return (
+        <div className={classes.typeGhost} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "dragon") {
+      return (
+        <div className={classes.typeDragon} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+    if (type === "dark") {
+      return (
+        <div className={classes.typeDark} key={i}>
+          <p>{type}</p>
+        </div>
+      );
+    }
+  });
 
   return (
     <div className="col-3 d-flex justify-content-evenly my-3" key={props.id}>
@@ -46,31 +163,22 @@ function EachCharacter(props) {
           </p>
         </div>
         <div className="card-body">
-          <h4 className="card-title">{props.name}</h4>
+          <div className={classes.orderNamePokebola}>
+            <h4 className="card-title">{props.name}</h4>
 
-          <div className={classes.typeStyle}>
-            {classType}
+            <div className={classes.pokebola}>
+              <div className={classes.detailPokebola}></div>
+            </div>
           </div>
 
-          {/* <div className={classes.typeStyle}>
-            {props.typePokemon.map((type, i) => (
-              <p key={i}>{type}</p>
-            ))}
-          </div> */}
+          <div className={classes.typeStyle}>{classType}</div>
 
-          <button className={classes.buttonStyle} onClick={showModalHandler}>Ver Pokemon</button>
+          <button className={classes.buttonStyle} onClick={showModalHandler}>
+            Pokedex
+          </button>
         </div>
       </div>
     </div>
-    // <div className={classes.container__cards}>
-    //   <div className={classes.card}>
-    //     <div className={classes.cover__card}>
-    //       <img src={props.image} alt={props.name} />
-    //     </div>
-    //     <h5>{props.name} - Nº {pokemonNumber.padStart(4, 0)}</h5>
-    //     <button onClick={showModalHandler}>Ver Pokemon</button>
-    //   </div>
-    // </div>
   );
 }
 
