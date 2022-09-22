04eec9419b9449ff39086d596c92235200700696
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-09-22 14:18:40 -0300

    upArrowSmooth

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,15 +13,27 @@ import PokemonRoutes from "./routes/PokemonRoutes";
 import { PokemonContextProvider } from "./context/charactersContext";
 import { UserProvider } from "./context/userContext";
 
+// Hook
+import { useRef } from "react";
+
+
 const App = () => {
+  const headerRef = useRef();
+
+  const upScrollHandler = () => {
+    console.log(headerRef)
+    headerRef.current.scrollIntoView({behavior: "smooth"});
+  }
   return (
     <UserProvider>
       <BrowserRouter>
         <PokemonContextProvider>
-          <Header />
+          <div ref={headerRef}>
+            <Header />
+          </div>
           <main>
             <PokemonRoutes />
-            <div className={classes.arrowUpContainer}>
+            <div onClick={upScrollHandler} className={classes.arrowUpContainer}>
               <i className="fa-solid fa-arrow-up"></i>
             </div>
           </main>
