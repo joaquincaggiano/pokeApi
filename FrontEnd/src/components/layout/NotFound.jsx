c5e828bf59cca06e82a49f68c969f2f9299e4e94
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-10-18 11:31:02 -0300

    css notFound

--- FrontEnd/src/components/layout/NotFound.jsx
+++ FrontEnd/src/components/layout/NotFound.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+// Css
+import classes from "./NotFound.module.css";
+
+// Image stop
+import pikachuStop from "../../img/pokeStop.png";
 
 function NotFound(props) {
   return (
-    <div className="text-white bordered">
-        <h1>404 | Pokemon Not Found</h1>
-        {props.search && <h3>{props.search} is not a Pokemon</h3>}
+    <div className={classes.notFoundBox}>
+      <h1>404 | Pokemon Not Found</h1>
+      {props.search && <h3>{props.search} is not a Pokemon</h3>}
+      <img className={classes.imgStop} src={pikachuStop} alt="pikachuStop" />
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
